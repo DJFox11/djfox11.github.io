c21700c4538a087fe7c72084be6bf6b29ffa3efb
DJFox11 <evanmcnicol05@gmail.com>
2024-04-23 18:55:18 +1000

    Arrow key support for ripping page.

--- ripping-tuts/scripts/ripping.js
+++ ripping-tuts/scripts/ripping.js
@@ -1,21 +1,41 @@
-$('.slide-nav').on('click', function(e) {
-    e.preventDefault();
-    // get current slide
-    var current = $('.flex--active').data('slide'),
-      // get button data-slide
-      next = $(this).data('slide');
-  
-    $('.slide-nav').removeClass('active');
-    $(this).addClass('active');
-  
-    if (current === next) {
-      return false;
-    } else {
-      $('.slider__warpper').find('.flex__container[data-slide=' + next + ']').addClass('flex--preStart');
-      $('.flex--active').addClass('animate--end');
-      setTimeout(function() {
-        $('.flex--preStart').removeClass('animate--start flex--preStart').addClass('flex--active');
-        $('.animate--end').addClass('animate--start').removeClass('animate--end flex--active');
-      }, 800);
-    }
+$(document).ready(function() {
+  var isAnimating = false;
+
+  $('.slide-nav').on('click', function(e) {
+      e.preventDefault();
+      if (isAnimating) return;
+
+      var current = $('.flex--active').data('slide'),
+          next = $(this).data('slide');
+
+      if (current === next) {
+          return false;
+      } else {
+          isAnimating = true;
+          $('.slide-nav').removeClass('active');
+          $(this).addClass('active');
+          $('.slider__warpper').find('.flex__container[data-slide=' + next + ']').addClass('flex--preStart');
+          $('.flex--active').addClass('animate--end');
+          setTimeout(function() {
+              $('.flex--preStart').removeClass('animate--start flex--preStart').addClass('flex--active');
+              $('.animate--end').addClass('animate--start').removeClass('animate--end flex--active');
+              isAnimating = false;
+          }, 800);
+      }
   });
+
+  $(document).keydown(function(e) {
+      if (isAnimating) return;
+      if (e.keyCode === 38) {
+          e.preventDefault();
+          var currentSlideIndex = $('.slide-nav.active').index();
+          var prevSlideIndex = currentSlideIndex === 0 ? $('.slide-nav').length - 1 : currentSlideIndex - 1;
+          $('.slide-nav').eq(prevSlideIndex).click();
+      } else if (e.keyCode === 40) {
+          e.preventDefault();
+          var currentSlideIndex = $('.slide-nav.active').index();
+          var nextSlideIndex = currentSlideIndex === ($('.slide-nav').length - 1) ? 0 : currentSlideIndex + 1;
+          $('.slide-nav').eq(nextSlideIndex).click();
+      }
+  });
+});
