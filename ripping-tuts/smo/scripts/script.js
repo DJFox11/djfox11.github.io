dd0c444eed3d34023ef8a67e6857a4d9aa598da7
DJFox11 <evanmcnicol05@gmail.com>
2024-06-04 21:23:54 +1000

    Update
    - Changed site font
    - Finished layout for SMO tutorial
    - Added back arrow to tutorial

--- ripping-tuts/smo/scripts/script.js
+++
@@ -1,9 +0,0 @@
-// Add target="_blank" to all external links
-document.addEventListener('DOMContentLoaded', function () {
-    var links = document.querySelectorAll('a');
-    links.forEach(function (link) {
-        if (link.hostname !== window.location.hostname) {
-            link.setAttribute('target', '_blank');
-        }
-    });
-});
